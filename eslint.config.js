db2bcaf4c6d22cb435510e8e2ddb1d2e60d12f2b
ne0pride <egor.ignatiew2017@yandex.ru>
2026-04-23 11:53:41 +0500

    testcommit

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,23 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import perfectionist from 'eslint-plugin-perfectionist';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
 
-export default defineConfig([
-  globalIgnores(['dist']),
+/** @type {import('eslint').Linter.Config} */
+const config = [
+  { ignores: ['*.config.*', 'dist', 'node_modules', 'package*.json', 'public'] },
+  js.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
+  reactHooks.configs['recommended-latest'],
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -22,8 +27,76 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    plugins: {
+      perfectionist, 
+      react,
+      'react-refresh': reactRefresh,
+      'unused-imports': unusedImports,
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': 'off',
+      'import/no-unresolved': 'error',
+      'import/no-unused-modules': 'error',
+      'import/order': 'off',
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          type: 'alphabetical',
+          order: 'asc',
+          groups: [
+            'value-builtin',
+            'value-internal',
+            ['value-parent', 'value-sibling'],
+            [
+              'type-import',
+              'type-internal',
+              'type-parent',
+              'type-sibling',
+              'type-index'
+             ],
+            'ts-equals-import',
+            'side-effect-style',
+            'style',
+          ],
+          internalPattern: [
+            '^/',
+            '^@components/',
+            '^@contexts/',
+            '^@hooks/',
+            '^@pages/',
+            '^@services/',
+            '^@utils/',
+          ],
+          newlinesBetween: 'ignore',
+        },
+      ],
+      'react/jsx-uses-react': 'off',
+      'react/prop-types': 'off',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        alias: {
+          map: [
+            ['', './public'],
+            ['@components', './src/components'],
+            ['@contexts', './src/contexts'],
+            ['@hoc/', './src/hoc'],
+            ['@hooks', './src/hooks'],
+            ['@pages', './src/pages'],
+            ['@services', './src/services'],
+            ['@utils', './src/utils'],
+          ],
+        },
+      },
     },
   },
-])
+  eslintPluginPrettierRecommended,
+];
+
+export default config;
